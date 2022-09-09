191d6e294ef04624c545c72adcb401b02b48c910
fabianagsilva <bisilvag2@gmail.com>
2022-09-08 23:00:41 -0300

    Exercicios Laços de decisão em Java 08.09.22

---
+++ Java/lacos-de-decisao/Java/src/Exercicios/Exercicio1.java
@@ -0,0 +1,32 @@
+	/* 1) João comprou um microcomputador para controlar o rendimento diário de seu trabalho. 
+	Toda vez que ele traz um peso de tomate maior que o estabelecido pelo regulamento do 
+	estado de São Paulo (50 quilos) deve pagar um multa de R$ 4,00 por quilo excedente. 
+	João precisa que você faça um sistema que leia a variável P (peso de tomates) e 
+	verifique se há excesso. Se houver, gravar na variável E (Excesso) e na 
+	variável M o valor da multa que João deverá pagar. Caso contrário mostrar
+	tais variáveis com o conteúdo ZERO.*/
+
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio1{
+
+	public static void main(String[] args){
+		double p,e,m = 4.00, vM = 0.0;
+		
+		Scanner leia = new Scanner(System.in);
+		
+		System.out.println("Digite o peso: ");
+		p = leia.nextDouble();
+				
+		if (p <= 50) { 
+		System.out.println("O valor da multa é de: R$ " + vM);
+	} 
+		else if(p > 50);{ 
+		e = p - 50; 
+		vM = m * e;
+		System.out.println("O excesso de peso foi de: " + e + "Kg " + "\nO valor da multa é de: R$ " + vM + "\n");
+		}
+	}
+}
---
+++ Java/lacos-de-decisao/Java/src/Exercicios/Exercicio2.java
@@ -0,0 +1,37 @@
+/*2) Elabore um programa que leia a variável N, número de horas trabalhadas de um operário. 
+E calcule o salário sabendo-se que ele ganha R$ 10,00 por hora. Quando o número de horas exceder a 50. 
+Calcule o excesso de pagamento armazenando-o na variável E, cas "o contrário zerar tal variável. 
+A hora excedente de trabalho vale R$ 20,00. No final do processamento imprimir o salário total e o salário excedente.*/
+
+package Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio2 {
+
+	public static void main(String[] args){
+		
+		double salHora = 10.0, salarioNormal, salarioTotal, vHoraE = 20.0, salarioExtra;
+		int horaE = 50, excesso, numeroH;
+	
+		Scanner leia = new Scanner(System.in);
+
+		System.out.println("Digite a quantidade de horas trabalhadas: ");
+		numeroH = leia.nextInt();
+		
+		salarioNormal = numeroH * salHora;
+		excesso = numeroH - horaE;
+		
+		if (numeroH > horaE){
+			salarioExtra = excesso * vHoraE;
+			salarioNormal = (numeroH - excesso) * salHora;
+			salarioTotal = salarioExtra + salarioNormal;
+			System.out.println("Salário Normal: R$ " + salarioNormal + "\nSalário Extra: R$ " + salarioExtra + "\nSalário Total: R$ " +salarioTotal + "\n");
+			
+		}
+		else{ 
+			excesso = 0;
+			System.out.println("\nSalário: R$ " + salarioNormal + "\nSalário extra: R$ " + excesso + "\n");
+		}
+	}
+}
